4d0e15effee0710d8ea5e9768a3d25f5a66b1ded
zach-he <971499086@qq.com>
2020-07-25 10:49:07 +0800

    [zach]feature:implement should_return_2A2B_when_answer_is_1234_given_1243 test

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -8,8 +8,31 @@ public class GuessNumberGame {
     }
 
     public String guess(String guess) {
-        if (guess == "1234")
-            return "4A0B";
-        return "0A0B";
+        int rightNumber = getRightNumber(guess);
+        int rightPositionNumber = getRightPositionNumber(guess);
+
+        return rightPositionNumber + "A" + (rightNumber - rightPositionNumber) + "B";
+    }
+
+    private int getRightNumber(String guess) {
+        int count = 0;
+        for (int i = 0; i < guess.length(); i++) {
+            for (int j = 0; j< guess.length(); j++) {
+                if(guess.charAt(i) == this.answer.charAt(j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private int getRightPositionNumber(String guess) {
+        int count = 0;
+        for (int i = 0; i < guess.length(); i++) {
+            if (guess.charAt(i) == this.answer.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
