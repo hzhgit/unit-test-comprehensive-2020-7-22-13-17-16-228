60b87f0025106840d135febe9b2ca319aa7d87df
zach-he <971499086@qq.com>
2020-07-25 10:30:14 +0800

    [zach]feature:implement should_return_0A0B_when_answer_is_1234_given_5678 test,and modify variable name

--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -6,15 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessNumberGameTest {
     @Test
-    public void should_4A0B_when_answer_is_1234_given_1234() {
+    public void should_return_4A0B_when_answer_is_1234_given_1234() {
         //given
-        String number = "1234";
+        String guess = "1234";
         GuessNumberGame guessNumberGame = new GuessNumberGame();
 
         //when
-        String actual = guessNumberGame.guess(number);
+        String actual = guessNumberGame.guess(guess);
 
         //then
         assertEquals("4A0B",actual);
+    }
+
+    @Test
+    public void should_return_0A0B_when_answer_is_1234_given_5678(){
+        //given
+        String guess = "5678";
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+
+        //when
+        String actual = guessNumberGame.guess(guess);
+
+        //then
+        assertEquals("0A0B",actual);
     }
 }
