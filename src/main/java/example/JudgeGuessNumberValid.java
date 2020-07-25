ff48cebe2d5457786149f7be9dec3c28dc122a68
zach-he <971499086@qq.com>
2020-07-25 12:15:06 +0800

    fixup! [zach]feature:implement should_false_when_judge_guess_number_valid_given_1123 and  should_false_when_judge_guess_number_valid_given_11

--- src/main/java/example/JudgeGuessNumberValid.java
+++ src/main/java/example/JudgeGuessNumberValid.java
@@ -1,8 +1,11 @@
 package example;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class JudgeGuessNumberValid implements JudgeNumberValid{
     @Override
     public boolean judgeGuessValid(String number) {
-        return false;
+        return new HashSet<>(Arrays.asList(number.split(""))).size() == 4;
     }
 }
