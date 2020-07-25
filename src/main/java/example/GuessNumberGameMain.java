91c1b111c85751ef48f195fe8121a6c610900cfc
zach-he <971499086@qq.com>
2020-07-25 12:46:21 +0800

    [zach]feature:implement the game

---
+++ src/main/java/example/GuessNumberGameMain.java
@@ -0,0 +1,39 @@
+package example;
+
+import java.util.Scanner;
+
+public class GuessNumberGameMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //generate answer
+        AnswerNumberGenerator answerNumberGenerator = new AnswerNumberGenerator();
+        String answer = answerNumberGenerator.generateAnswer();
+        // judge guess number is valid
+        JudgeGuessNumberValid judgeGuessNumberValid = new JudgeGuessNumberValid();
+        // init the game
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+
+        System.out.println("answer : " + answer);
+
+        int count = 0;
+        while(sc.hasNextLine()){
+            String guess = "";
+            guess += sc.next();
+            count++;
+            if(judgeGuessNumberValid.judgeGuessValid(guess)){
+                String result = guessNumberGame.guess(guess);
+                if(result.equals("4A0B")){
+                    System.out.println(result);
+                    break;
+                }else{
+                    System.out.println(result);
+                }
+            }else{
+                System.out.println("Wrong Input£¬Input again");
+            }
+
+            if(count == 6)
+                break;
+        }
+    }
+}
