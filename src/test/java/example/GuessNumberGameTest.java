e4fbf491502bf201912db7f7b2b27bbf5f8478ee
zach-he <971499086@qq.com>
2020-07-25 11:20:40 +0800

    [zach]refactor:use mock modified all the test

--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -14,8 +14,9 @@ public class GuessNumberGameTest {
     public void should_return_4A0B_when_answer_is_1234_given_1234() {
         //given
         String guess = "1234";
-        String answer = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        NumberGenerator numberGenerator = Mockito.mock(NumberGenerator.class);
+        when(numberGenerator.generateAnswer()).thenReturn("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(numberGenerator.generateAnswer());
 
         //when
         String actual = guessNumberGame.guess(guess);
@@ -28,8 +29,9 @@ public class GuessNumberGameTest {
     public void should_return_0A0B_when_answer_is_1234_given_5678() {
         //given
         String guess = "5678";
-        String answer = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        NumberGenerator numberGenerator = Mockito.mock(NumberGenerator.class);
+        when(numberGenerator.generateAnswer()).thenReturn("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(numberGenerator.generateAnswer());
 
         //when
         String actual = guessNumberGame.guess(guess);
@@ -42,8 +44,9 @@ public class GuessNumberGameTest {
     public void should_return_2A2B_when_answer_is_1234_given_1243() {
         //given
         String guess = "1243";
-        String answer = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        NumberGenerator numberGenerator = Mockito.mock(NumberGenerator.class);
+        when(numberGenerator.generateAnswer()).thenReturn("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(numberGenerator.generateAnswer());
 
         //when
         String actual = guessNumberGame.guess(guess);
@@ -56,8 +59,9 @@ public class GuessNumberGameTest {
     public void should_return_1A1B_when_guess_given_is_1025_and_answer_1234() {
         //given
         String guess = "1025";
-        String answer = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        NumberGenerator numberGenerator = Mockito.mock(NumberGenerator.class);
+        when(numberGenerator.generateAnswer()).thenReturn("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(numberGenerator.generateAnswer());
 
         //when
         String actual = guessNumberGame.guess(guess);
@@ -70,8 +74,9 @@ public class GuessNumberGameTest {
     public void should_return_0A4B_when_guess_given_is_4321_and_answer_1234() {
         //given
         String guess = "4321";
-        String answer = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        NumberGenerator numberGenerator = Mockito.mock(NumberGenerator.class);
+        when(numberGenerator.generateAnswer()).thenReturn("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(numberGenerator.generateAnswer());
 
         //when
         String actual = guessNumberGame.guess(guess);
@@ -84,8 +89,9 @@ public class GuessNumberGameTest {
     public void should_return_0A2B_when_guess_given_is_2107_and_answer_1234() {
         //given
         String guess = "2107";
-        String answer = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+        NumberGenerator numberGenerator = Mockito.mock(NumberGenerator.class);
+        when(numberGenerator.generateAnswer()).thenReturn("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(numberGenerator.generateAnswer());
 
         //when
         String actual = guessNumberGame.guess(guess);
