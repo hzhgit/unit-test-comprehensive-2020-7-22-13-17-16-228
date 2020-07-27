26c7816aa43a212f6a11f2680406cbb5edc6ae32
zach-he <971499086@qq.com>
2020-07-27 20:11:37 +0800

    [zach]refactor:modify judgeGuessValid mothod

--- src/main/java/example/JudgeGuessNumberValid.java
+++ src/main/java/example/JudgeGuessNumberValid.java
@@ -6,6 +6,9 @@ import java.util.HashSet;
 public class JudgeGuessNumberValid implements JudgeNumberValid{
     @Override
     public boolean judgeGuessValid(String number) {
+        if(number.length() != 4 || !(number.matches("[0-9]+"))) {
+            return false;
+        }
         return new HashSet<>(Arrays.asList(number.split(""))).size() == 4;
     }
 }
