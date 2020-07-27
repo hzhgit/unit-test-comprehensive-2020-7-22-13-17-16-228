9c9db2220c9584d72d3d5b8d33e4286ea9f0fab2
zach-he <971499086@qq.com>
2020-07-27 20:17:20 +0800

    [zach]refactor:modify String to StringBuilder

--- src/main/java/example/AnswerNumberGenerator.java
+++ src/main/java/example/AnswerNumberGenerator.java
@@ -6,14 +6,14 @@ public class AnswerNumberGenerator implements NumberGenerator {
 
     @Override
     public String generateAnswer() {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         Random random = new Random();
 
         while (answer.length() < 4) {
             int num = random.nextInt(10);
-            if (!answer.contains(String.valueOf(num)))
-                answer += String.valueOf(num);
+            if (!answer.toString().contains(String.valueOf(num)))
+                answer.append(String.valueOf(num));
         }
-        return answer;
+        return answer.toString();
     }
 }
