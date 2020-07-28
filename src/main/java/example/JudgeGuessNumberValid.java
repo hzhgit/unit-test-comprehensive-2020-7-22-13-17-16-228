37482788e73bb92d0ae7501347d863aa41660d3c
hzhgit <971499086@qq.com>
2020-07-28 12:48:10 +0800

    [zach]refactor:extract "[0-9]" + to a constant

--- src/main/java/example/JudgeGuessNumberValid.java
+++ src/main/java/example/JudgeGuessNumberValid.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class JudgeGuessNumberValid implements JudgeNumberValid{
+
+    private static final String REGULAR_EXPRESSION_FOR_A_NUMBER_STRING = "[0-9]+";
+
     @Override
     public boolean judgeGuessValid(String numberString) {
-        //TODO magic number,正则
-        if(numberString.length() != 4 || !(numberString.matches("[0-9]+"))) {
+        if(numberString.length() != 4 || !(numberString.matches(REGULAR_EXPRESSION_FOR_A_NUMBER_STRING))) {
             return false;
         }
         return new HashSet<>(Arrays.asList(numberString.split(""))).size() == 4;
