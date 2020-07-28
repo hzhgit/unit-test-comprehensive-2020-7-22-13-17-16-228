d35b53d645c51c9b2274eacd536fae8d4a82bea9
hzhgit <971499086@qq.com>
2020-07-28 13:05:29 +0800

    [zach]refactor:extract 4 to constant

--- src/main/java/example/JudgeGuessNumberValid.java
+++ src/main/java/example/JudgeGuessNumberValid.java
@@ -6,12 +6,13 @@ import java.util.HashSet;
 public class JudgeGuessNumberValid implements JudgeNumberValid{
 
     private static final String REGULAR_EXPRESSION_FOR_A_NUMBER_STRING = "[0-9]+";
+    private static final int DEFAULT_NUMBER_STRING_LENGTH = 4;
 
     @Override
     public boolean judgeGuessValid(String numberString) {
         if(numberString.length() != 4 || !(numberString.matches(REGULAR_EXPRESSION_FOR_A_NUMBER_STRING))) {
             return false;
         }
-        return new HashSet<>(Arrays.asList(numberString.split(""))).size() == 4;
+        return new HashSet<>(Arrays.asList(numberString.split(""))).size() == DEFAULT_NUMBER_STRING_LENGTH;
     }
 }
